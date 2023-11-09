298bc0e80f457a304e9d4f618d4f03ab5657fd16
RodrigoSenaGit <senapkd10@gmail.com>
2023-11-09 19:41:34 -0300

    first commit

---
+++ main.java
@@ -0,0 +1,21 @@
+public class main {
+    public static void main(String[] args) {
+        String nome = "Senna";
+
+
+        System.out.println("ola , " + nome + "Vamos fazer um calculo: ");
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        int subtração = a - b;
+        float divisão = (float) a / b ;
+        int mutiplicação = a * b;
+        
+        System.out.println("a + b = " + soma);
+        System.out.println("a - b = " + subtração);
+        System.out.println("a / b = " + divisão);
+        System.out.println("a * b = " + mutiplicação);
+        
+    }
+}
